40bf8be56ccf32590d713b9c90c7beb12ba6decc
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2022-10-23 20:39:09 +0300

    questions

--- js/primitiveDB.js
+++ js/primitiveDB.js
@@ -2,8 +2,24 @@ import inquirer from 'inquirer';
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
+    {
+      type: 'input',
+      name: 'user',
+      message: 'Pls, enter the user name or press ENTER to cancel:',
+    },
+    {
+      type: 'list',
+      name: 'gender',
+      message: 'Pls, choose the gender:',
+      choices: ['male', 'female'],
+    },
+    {
+      type: 'input',
+      name: 'age',
+      message: 'Pls, enter the age:',
+    },
   ])
   .then(answers => {
     // Use user feedback for... whatever!!
+    console.log(answers);
   });
