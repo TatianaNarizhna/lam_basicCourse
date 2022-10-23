0756dcab9d8f4ade6b2b781d62a593f9abafa47e
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2022-10-23 22:49:26 +0300

    add validator

--- js/primitiveDB.js
+++ js/primitiveDB.js
@@ -1,24 +1,40 @@
 import inquirer from 'inquirer';
 
+const questions = [
+  {
+    type: 'input',
+    name: 'user',
+    message: 'Pls, enter the user name or press ENTER to cancel:',
+    validate: answer => {
+      if (answer === '') {
+        return 'Pls, enter a valid name';
+      }
+      return true;
+    },
+  },
+  {
+    type: 'list',
+    name: 'gender',
+    message: 'Pls, choose the gender:',
+    choices: ['male', 'female'],
+    default: 'male',
+  },
+  {
+    type: 'input',
+    name: 'age',
+    message: 'Pls, enter the age:',
+    validate: answer => {
+      if (Number.isNaN(answer)) {
+        return 'Pls, enter a valid age';
+      }
+      return true;
+    },
+  },
+];
+
 inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'user',
-      message: 'Pls, enter the user name or press ENTER to cancel:',
-    },
-    {
-      type: 'list',
-      name: 'gender',
-      message: 'Pls, choose the gender:',
-      choices: ['male', 'female'],
-    },
-    {
-      type: 'input',
-      name: 'age',
-      message: 'Pls, enter the age:',
-    },
-  ])
+  .prompt(questions)
+
   .then(answers => {
     // Use user feedback for... whatever!!
     console.log(answers);
