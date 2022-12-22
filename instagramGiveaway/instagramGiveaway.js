bad93c3350a7be60c85cf8dd7884c2bf66d8ab3a
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2022-12-22 21:53:14 +0200

    tlg

--- instagramGiveaway/instagramGiveaway.js
+++ instagramGiveaway/instagramGiveaway.js
@@ -1,39 +1,104 @@
-import fs from 'fs/promises';
+import fs from 'fs';
 import path from 'path';
 import * as url from 'url';
+import intersection from 'lodash.intersection';
+
+const repoToRead = './instagramGiveaway/data';
+const argv = process.argv;
 
 const __dirname = url.fileURLToPath(new URL('./data', import.meta.url));
 
-const repoToRead = './instagramGiveaway/data';
+const files = fs.readdirSync(__dirname)
 
-const argv = process.argv;
+function uniqueValues (filesArr) {
+   let uniqueValue = new Set()
+   filesArr.forEach(file => {
+      const filePath = path.join(__dirname, file);
+      const array = fs.readFileSync(filePath, 'utf-8').toString().split("\n")
+  
+     for (const key in array) { 
+      uniqueValue.add(array[key]) ; 
+   }
+   });
+   return `Unique phrases: ${uniqueValue.size}`
+}
 
-const onRepoToRead = async rep => {
-  try {
-    const result = await fs.readdir(rep);
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
-};
 
-const onFileToRead = async () => {
-  try {
-    const fileText = await onRepoToRead(repoToRead);
+function existInAllFiles(filesArr) {
+  let fullValuesArray = new Array()
+  filesArr.forEach(file => {
+    const filePath = path.join(__dirname, file);
+    const array = fs.readFileSync(filePath, 'utf-8').toString().split("\n")
+    fullValuesArray.push(array)
+  })
+  let existValues = intersection(fullValuesArray )
 
-    const txt = fileText.forEach(file => {
-      const dataPath = path.join(__dirname, file);
-      return dataPath;
-    });
-    await fs.readFile(txt, 'utf-8');
-    console.log(txt);
-    return txt;
-  } catch (error) {
-    console.error(error);
-  }
-};
+  return `Exist in all files: ${existValues.length}`
 
-onFileToRead();
+
+}
+
+console.log(uniqueValues (files))
+console.log(existInAllFiles(files));
+
+// console.time('1');
+// const start= new Date().getTime();
+// existInAllFiles(files)
+// const end = new Date().getTime();
+// console.log(`SecondWay: ${end - start}ms`);
+// console.timeEnd('1')
+
+
+// Execution time
+// uniqueValues: 61-62ms
+// existInAllFiles: 42-44ms
+
+// Results
+// uniqueValues:
+// existInAllFiles: 
+
+
+
+// function uniqueValues() {
+//   const data = onGetData(repoToRead)
+//   console.log(data);
+
+//   // const uniqueNames = data.filter((el, index, arr) => arr.indexOf(el) === index)
+//   // console.log(uniqueNames)
+//   // return uniqueNames;
+
+// }
+
+// uniqueValues()
+
+
+
+// const onRepoToRead = async rep => {
+//   try {
+//     const result = await fs.readdir(rep);
+//     return result;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// const onFileToRead = async () => {
+//   try {
+//     const fileText = await onRepoToRead(repoToRead);
+
+//     const txt = fileText.forEach(file => {
+//       const dataPath = path.join(__dirname, file);
+//       return dataPath;
+//     });
+//     await fs.readFile(txt, 'utf-8');
+//     console.log(txt);
+//     return txt;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// onFileToRead();
 
 // const onMakeArray = async () => {
 //   try {
@@ -49,21 +114,21 @@ onFileToRead();
 
 // resultingArray();
 
-// fs.readdir(repoToRead, (err, files) => {
-//   if (err) {
-//     onError(err);
-//     return;
-//   }
-//   files.forEach(file => {
-//     const dataPath = path.join(__dirname, file);
 
-//     fs.readFile(dataPath, 'utf-8', (err, content) => {
-//       if (err) {
-//         console.log(err);
-//         return;
-//       }
-//       const array = content.toString().split('\n');
-//       console.log(array);
-//     });
-//   });
-// });
+
+  //  fls.forEach(file => {
+  //     const dataPath = path.join(__dirname, file);
+
+  //     fs.readFileSync(dataPath, 'utf-8', (err, content) => {
+  //       if (err) {
+  //         console.error(err);
+  //         return;
+  //       }
+  //    console.log(content);
+  //       const array = content.toString().split('\n');
+  //       // let pop = array.pop()
+  //       // let res = new Set(array).size
+   
+  //       console.log(array);
+  //     });
+  //   });
