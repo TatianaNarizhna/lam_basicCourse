8c164f143caf08c2e89579fc0da558b03ed48a05
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2023-01-05 22:47:37 +0200

    fff

--- geolocationIP/geolocationIP.js
+++ geolocationIP/geolocationIP.js
@@ -5,26 +5,52 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
-app.set('trust proxy', true);
+// app.use(bodyParser.json());
+// app.set('trust proxy', true);
 
 const __dirname = url.fileURLToPath(new URL('./IPDB.CSV', import.meta.url));
 let jsonData = fs.readFileSync(__dirname, 'utf8').split('\r\n');
-// console.log(jsonData);
 
-function oneEl(data) {
-  let ipArr = [];
+function oneEl(data, ip) {
+  const arrEl = [];
+  const yyy = ip.split('.');
+
+  let ip2 =
+    yyy[0] * 256 ** 3 +
+    yyy[1] * 256 ** 2 +
+    yyy[2] * 256 ** 1 +
+    yyy[3] * 256 ** 0;
+  console.log(+ip);
+
   for (let index = 0; index < data.length; index++) {
     const element = data[index];
-
-    const strEl = element.replace(/["]/g, '').split(',');
-    const ip = strEl.slice(0, 2);
-    ipArr.push(Number(...ip));
+    const dataEl = element.replace(/["]/g, '').split(',');
+    arrEl.push(dataEl);
+    // const ip = strEl.slice(0, 2);
   }
-  return ipArr;
+
+  for (const el of arrEl) {
+    let ip1 = Number(el.slice(0, 1));
+    let ip2 = Number(el.slice(1, 2));
+    let location = el.slice(2, 3);
+
+    if (ip >= ip1 && ip <= ip2) {
+      res.send(`<h2>Your location ${location}</h2>`);
+    }
+  }
+
+  return arrEl;
 }
 
 // oneEl(jsonData);
+
+// app.get('/', (req, res) => {
+//   res.send('<h1></h2>');
+//   var ip = req.headers['x-forwarded-for'];
+//   console.log(ip);
+//   req.remoteAddress;
+//   req.socket.remoteAddress || req.socket.remoteAddress;
+// });
 
 app.get('/', (req, res) => {
   res.send('<h1>Geolocation says Hello</h1>');
@@ -32,12 +58,22 @@ app.get('/', (req, res) => {
 
 app.get('/get_ip', (req, res) => {
   const ipAddress = req.header('x-forwarded-for') || req.socket.remoteAddress;
+
   res.send(`<h2>Your IP ${ipAddress}</h2>`);
 });
 
 app.get('/get_location', (req, res) => {
-  const el = oneEl(jsonData);
-  console.log(el);
+  res.send('<h1></h1>');
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  req.remoteAddress;
+  req.socket.remoteAddress || req.socket.remoteAddress;
+
+  oneEl(jsonData, ip);
+});
+
+app.listen(port, () => {
+  console.log(`example app listening on port ${port}`);
 });
 
 // app.get('/', (req, res) => {
@@ -53,7 +89,3 @@ app.get('/get_location', (req, res) => {
 
 //   finding(elements, ip);
 // });
-
-app.listen(port, () => {
-  console.log(`example app listening on port ${port}`);
-});
