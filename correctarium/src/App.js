85dc2e4647f4969d076607f14b218d11d0d9c40a
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2023-01-12 19:06:49 +0200

    react

---
+++ correctarium/src/App.js
@@ -0,0 +1,12 @@
+import FormElement from "./components/Form"
+import Section from "./components/Section"
+
+export default function App () {
+return (
+    <>
+      <Section title="Замовити переклад або редагування"> 
+        <FormElement/>
+      </Section>
+    </>
+)
+}
---
+++ correctarium/src/components/Form.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+
+ function FormElement () {
+    return (
+        <div>
+            <form action=""></form>
+        </div>
+    )
+}
+
+FormElement.propTypes = {
+    
+}
+
+export default FormElement;
