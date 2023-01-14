0f93d257d45f16eaa22ffd2c87cda9cfa79183f2
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2023-01-14 23:41:16 +0200

    form

--- correctarium/src/components/Form/Form.js
+++ correctarium/src/components/Form/Form.js
@@ -1,15 +1,70 @@
-import PropTypes from 'prop-types'
+import { Component } from 'react';
+import PropTypes from 'prop-types';
 
- function FormElement () {
+class FormElement extends Component {
+  state = {
+    service: '',
+    textField: '',
+  };
+
+  handleServiceChange = e => {
+    this.setState({ service: e.currentTarget.value });
+  };
+
+  handleTextFieldChange = e => {
+    this.setState({ textField: e.currentTarget.value });
+  };
+
+  render() {
     return (
-        <div>
-            <form action=""></form>
-        </div>
-    )
+      <div>
+        <form action="">
+          <label htmlFor="service">Послуга</label>
+          <input
+            list="services"
+            id="service"
+            name="service"
+            value={this.state.service}
+            onChange={this.handleServiceChange}
+          />
+          <datalist id="services">
+            <option value="Переклад" />
+            <option value="Редагування" />
+          </datalist>
+
+          <div>
+            <textarea
+              name="text"
+              id=""
+              cols="40"
+              rows="10"
+              placeholder="Введіть текст або загрузіть файл "
+              value={this.state.textField}
+              onChange={this.handleTextFieldChange}
+            ></textarea>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-FormElement.propTypes = { 
-    
-}
+//  function FormElement () {
+//     return (
+//         <div>
+//             <form action="">
+//                 <label htmlFor="service">Послуга</label>
+//                 <input list= "services" id="service" name='service' />
+//              <datalist id='services'>
+//              <option value="Переклад"/>
+//              <option value="Редагування"/>
+//              </datalist>
+
+//             </form>
+//         </div>
+//     )
+// }
+
+FormElement.propTypes = {};
 
 export default FormElement;
