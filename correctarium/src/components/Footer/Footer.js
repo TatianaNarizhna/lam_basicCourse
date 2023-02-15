567626cb8c242aaec44f13528871a71627eedc25
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2023-02-15 13:30:41 +0200

    markUp

--- correctarium/src/components/Footer/Footer.js
+++ correctarium/src/components/Footer/Footer.js
@@ -6,7 +6,9 @@ function Footer() {
     <footer className={s.footer_bkgr}>
       <div className={s.footer_styles}>
         <div className={s.footer_rights}>
-          <a href="https://correctarium.com/terms">Договір публічної оферти</a>
+          <a className={s.terms} href="https://correctarium.com/terms">
+            Договір публічної оферти
+          </a>
           <p>© Correctarium</p>
           <p>2015-2023</p>
         </div>
@@ -14,7 +16,9 @@ function Footer() {
 
         <div className={s.footer_contacts}>
           <p>Надіслати текст на переклад:</p>
-          <a href="mailto:manager@correctarium.com">manager@correctarium.com</a>
+          <a className={s.email} href="mailto:manager@correctarium.com">
+            manager@correctarium.com
+          </a>
         </div>
       </div>
     </footer>
