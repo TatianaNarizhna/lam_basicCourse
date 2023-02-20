84b1cc2916da50a202251dc2cd8b4d88a8346cb5
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2023-02-20 22:05:51 +0200

    auth

--- authorization/src/auth/authOperation.js
+++ authorization/src/auth/authOperation.js
@@ -11,11 +11,18 @@ const token = {
   },
 };
 
-async function getToken() {
+async function signup() {
   try {
-    const response = await axios.get('/me');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
+    const response = await axios.post('/sign_up ');
+    // token.set(data.token);
+    console.log(JSON.stringify(response.data));
+  } catch ({ response }) {
+    console.error(response.data.message);
   }
 }
+
+const authOperations = {
+  signup,
+};
+
+export default authOperations;
