bf5a3e36799221739851a106f375b26fe8cce270
TatianaNarizhna <73964053+TatianaNarizhna@users.noreply.github.com>
2023-03-04 16:58:52 +0200

    js

--- tsArrayCustom/methods.js
+++ tsArrayCustom/methods.js
@@ -118,7 +118,63 @@ Array.prototype.flatten = function (array) {
   return result;
 };
 
-// console.log(Array.prototype.associateBy(['fff', 'ggg', 'rrr']));
+Array.prototype.fold = function (array) {
+  let result = 0;
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    result += element;
+  }
+  return result;
+};
+
+Array.prototype.maxBy = function (array) {
+  let max = array[0];
+  for (let index = 0; index < array.length; index++) {
+    const currElement = array[index];
+    const maxElement = max;
+
+    if (currElement > maxElement) {
+      max = currElement;
+    }
+  }
+  return max;
+};
+
+Array.prototype.minBy = function (array) {
+  let max = array[0];
+  for (let index = 0; index < array.length; index++) {
+    const currElement = array[index];
+    const maxElement = max;
+
+    if (currElement < maxElement) {
+      max = currElement;
+    }
+  }
+  return max;
+};
+
+Array.prototype.count = function (array) {
+  let ttl = 0;
+  for (const element of array) {
+    let population = element.population;
+    ttl += population;
+  }
+  return ttl;
+};
+
+Array.prototype.groupBy = function (array) {
+  let groups = {};
+  for (const element of array) {
+    const key = element.rating;
+
+    if (key in groups) {
+      groups[key].push(element);
+    } else {
+      groups[key] = element;
+    }
+  }
+  return groups;
+};
 
 const predicate = x => x.length > 1;
 const filterPredicate = x => x > 3;
@@ -126,6 +182,23 @@ const arr = ['moon', 'sun', 'star', 'cloud', 'planet'];
 const array = [1, 2, 3, 4, 5];
 const arrayDistinctBy = ['book', 'pencil', 'pen', 'laptop', 'book', 'pen'];
 const arrFlatten = [1, [2, 3], [4, [5, 6]]];
+const count = [
+  {
+    country: 'NL',
+    population: 15000,
+    rating: 10,
+  },
+  {
+    country: 'BL',
+    population: 11000,
+    rating: 9,
+  },
+  {
+    country: 'UK',
+    population: 25000,
+    rating: 8,
+  },
+];
 
 console.log(arr.associateBy(arr));
 console.log(arr.all(arr, predicate));
@@ -139,6 +212,11 @@ console.log(array.filterNot(array, filterPredicate));
 console.log(array.find(array, filterPredicate));
 console.log(array.findLast(array, filterPredicate));
 console.log(arrFlatten.flatten(arrFlatten));
+console.log(array.fold(array));
+console.log(array.maxBy(array));
+console.log(array.minBy(array));
+console.log(count.count(count));
+console.log(count.groupBy(count));
 
 // Array.prototype.flatten = function (array) {
 //   let result = [];
